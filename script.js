790a5c4a7472d26e710e627054077394ad7ededb
Calvin-WD <calvinlamel@outlook.de>
2026-04-24 07:45:43 +0200

    build: delete basket function from script.js

--- script.js
+++ script.js
@@ -13,28 +13,6 @@ function init() {
   renderCategories();
 }
 
-// function addToBasket(dishId) {
-//   const dish = getDishFromDatabase(dishId);
-//   basket.dishes.push(dish);
-
-//   saveToLocalStorage(BASKET, basket.dishes);
-// }
-
-// function getDishFromDatabase(dishId) {
-//   let dish = "";
-
-//   for (let indexCat = 0; indexCat < menu.categories.length; indexCat++) {
-//     dish = menu.categories[indexCat].dishes.filter(
-//       (element) => element.id == dishId,
-//     );
-
-//     if (dish.length != 0) {
-//       return dish;
-//     }
-//   }
-//   return;
-// }
-
 function saveToLocalStorage(keyString, value) {
   localStorage.setItem(keyString, JSON.stringify(value));
 }
