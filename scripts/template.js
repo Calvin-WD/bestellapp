9809372b7705955336e567eaeccc8fd2c3dd0992
Calvin-WD <calvinlamel@outlook.de>
2026-05-04 10:28:41 +0200

    feat: implement template function for following buttons: 'addToBasket', 'removeFromBasket', QuantityAdditionButton, QuantitySubtractionButton

--- scripts/template.js
+++ scripts/template.js
@@ -19,20 +19,17 @@ export function getDishTemplate(dish, buttonText, buttonClass, buttonState) {
           </div>
           <div class="dish__price">
             <p>${dish.price.toFixed(2)}€</p>
-            <button
-              id="buttonAddToBasket-id${dish.id}"
-              type="button"
-              class="button${buttonClass}"
-              ${buttonState}
-              onclick="addToBasket(${dish.id}, this, 'append')">
-              ${buttonText}
-            </button>
+            ${getAddToBasketButtonTemplate(dish, buttonText, buttonClass, buttonState)}
           </div>
       </div>
     </article>`;
 }
 
-export function getBasketTemplate(basketDishesHtml, basketPriceHtml, scrollClass) {
+export function getBasketTemplate(
+  basketDishesHtml,
+  basketPriceHtml,
+  scrollClass,
+) {
   return `<article class="basket">
         <h2>Dein Warenkorb</h2>
         <section id="basketDishes-id" class="basket__dishesContainer ${scrollClass}">
@@ -53,35 +50,15 @@ export function getBasketDishEmptyTemplate() {
 
 export function getBasketDishTemplate(basketDish) {
   return `<div class="basket__dish">
-            <p>${basketDish.name}</p>
+            <div>
+              <p>${basketDish.name}</p>
+            </div>
             <div class="basket__dishPriceContainter">
               <div class="basket__buttonContainer">
-                <button
-                  id="subButton-${basketDish.id}"
-                  type="button"
-                  class="quantityBtn dNone"
-                  onclick="changeBasketDishAmount(${basketDish.id}, 'subtraction')">
-                    -
-                </button>
-                <button
-                  id="delButton-${basketDish.id}"
-                  type="button"
-                  class="quantityBtn quantityBtn--delete"
-                  onclick="removeFromBasket(${basketDish.id}, 'delete')">
-                    <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM13 3H3V16H13V3ZM6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14ZM10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14Z"
-                      stroke="currentColor"
-                      stroke-width="0.2"
-                      fill="currentColor"/>
-                    </svg>
-                </button>
+                ${getQuantitySubtractionButtonTemplate(basketDish)}
+                ${getDeleteButtonTemplate(basketDish)}
                 <p id="amount-${basketDish.id}">${basketDish.amount}</p>
-                <button
-                  type="button"
-                  class="quantityBtn"
-                  onclick="changeBasketDishAmount(${basketDish.id}, 'addition')">
-                    +
-                </button>
+                ${getQuantityAdditionButtonTemplate(basketDish)}
               </div>
               <p>${basketDish.price.toFixed(2)}€</p>
             </div>
@@ -111,4 +88,49 @@ export function getBasketPriceTemplate(basket) {
               <th id="total-id">${basket.summery.total.toFixed(2)}€</th>
             </tr>
           </table>`;
+}
+
+function getAddToBasketButtonTemplate(dish, buttonText, buttonClass, buttonState) {
+  return `<button
+            id="buttonAddToBasket-id${dish.id}"
+            type="button"
+            class="button${buttonClass}"
+            ${buttonState}
+            onclick="addToBasket(${dish.id}, this, 'append')">
+            ${buttonText}
+          </button>`;
+}
+
+function getDeleteButtonTemplate(basketDish) {
+  return `<button
+          id="delButton-${basketDish.id}"
+          type="button"
+          class="quantityBtn quantityBtn--delete"
+          onclick="removeFromBasket(${basketDish.id}, 'delete')">
+            <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM13 3H3V16H13V3ZM6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14ZM10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14Z"
+              stroke="currentColor"
+              stroke-width="0.2"
+              fill="currentColor"/>
+            </svg>
+         </button>`;
+}
+
+function getQuantityAdditionButtonTemplate(basketDish) {
+  return `<button
+            type="button"
+            class="quantityBtn"
+            onclick="changeBasketDishAmount(${basketDish.id}, 'addition')">
+              +
+          </button>`;
+}
+
+function getQuantitySubtractionButtonTemplate(basketDish) {
+  return `<button
+            id="subButton-${basketDish.id}"
+            type="button"
+            class="quantityBtn dNone"
+            onclick="changeBasketDishAmount(${basketDish.id}, 'subtraction')">
+              -
+          </button>`;
 }
