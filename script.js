0514f15874c1915d145ad7303e1629776b525a1b
Calvin-WD <calvinlamel@outlook.de>
2026-05-04 08:57:22 +0200

    feat: import functions; implement constants for get baskets localstorage data

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 /** import the javascript modules*/
 import { renderCategories, renderBasket } from "./scripts/render.js";
-import { addToBasket } from "./scripts/basket.js";
+import {
+  basket,
+  addToBasket,
+  removeFromBasket,
+  changeBasketDishAmount,
+} from "./scripts/basket.js";
 import { saveToLocalStorage, getFromLocalStorage } from "../scripts/saving.js";
-import { menu, basket } from "./scripts/db.js";
+import { menu } from "./scripts/db.js";
 
+const MENU = "Menu";
 const BASKET_DISHES = "Basket-Dishes";
 const BASKET_SUM = "Basket-Summery";
 
 function init() {
-  let currentBasketDishes = getFromLocalStorage(BASKET_DISHES);
-  let currentBasketSummery = getFromLocalStorage(BASKET_SUM);
+  // const currentMenu = getFromLocalStorage(MENU);
+  const currentBasketDishes = getFromLocalStorage(BASKET_DISHES);
+  const currentBasketSummery = getFromLocalStorage(BASKET_SUM);
 
   if (currentBasketDishes != null) {
     basket.dishes = currentBasketDishes;
@@ -24,5 +31,8 @@ function init() {
 
 window.init = init;
 window.addToBasket = addToBasket;
+window.removeFromBasket = removeFromBasket;
+window.changeBasketDishAmount = changeBasketDishAmount;
 window.menu = menu;
+
 window.basket = basket;
